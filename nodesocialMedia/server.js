66c37d932f8143f30fbab4f27716dfb918812a74
mayhim182 <mayankatnitrkl@gmail.com>
2023-12-11 20:58:38 +0530

    daily cp

---
+++ nodesocialMedia/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on port ${PORT}`);
+});
+
+module.exports = app;
+
