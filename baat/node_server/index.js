c3079f763cb4bb304077f0ea4b91fb043db38d68
mayhim182 <mayankatnitrkl@gmail.com>
2024-03-08 09:22:56 +0530

    daily code push

--- baat/node_server/index.js
+++ baat/node_server/index.js
@@ -1,14 +1,16 @@
-const io = require('socket.io')(8000);
+const io = require('socket.io')(8000,{ cors: { origin: '*' } });
+
 
 const users = {};
 
 io.on('connection',socket =>{
     socket.on('new-user-joined', name =>{
+        console.log(name,"joined");
         users[socket.id] = name;
         socket.broadcast.emit('user-joined',name);
     });
 
     socket.on('send',message=>{
-        socket.broadcast.emit('receive', {message:message, name:user[socket.id]})
+        socket.broadcast.emit('receive', {message:message, name:users[socket.id]})
     });
 })
